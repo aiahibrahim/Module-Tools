b4b77eca941ae8413813ee1952951823f758c4a5
Daniel Wagner-Hall <daniel@codeyourfuture.io>
2025-01-08 13:23:12 +0000

    Add exercises for individual shell tools and shell pipelines (#1)
    Achieving these exercises meets all of the shell tools learning
    objectives of the first two sprints.
    
    ---------
    
    Co-authored-by: Alasdair Smith <alasdairsmith100@gmail.com>

---
+++ individual-shell-tools/sed/script-01.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# TODO: Write a command to output input.txt with all occurrences of the letter `s` replaced with `S`.
+# The output should contain 11 lines.
+# The first line of the output should be: "ThiS iS a Sample file for experimenting with Sed.".
