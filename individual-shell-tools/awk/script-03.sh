7034b21a482383c27a2e1d2775eb5b981743bef6
aiah ibrahim <ayababikeribrahim@gmail.com>
2025-07-14 00:21:18 +0100

    AWK solved

--- individual-shell-tools/awk/script-03.sh
+++ individual-shell-tools/awk/script-03.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -euo pipefail
+awk '{print $1, $3}' scores-table.txt
 
 # TODO: Write a command to output just the names of each player along with the score from their first attempt.
 # Your output should contain 6 lines, each with one word and one number on it.
