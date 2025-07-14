96a077a9a9839c34771508a877b813c0c6a98215
aiah ibrahim <ayababikeribrahim@gmail.com>
2025-07-14 02:34:01 +0100

    SED solved

--- individual-shell-tools/sed/script-03.sh
+++ individual-shell-tools/sed/script-03.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -euo pipefail
-
+sed '/[0-9]/d' input.txt
 # TODO: Write a command to output input.txt removing any line which contains a number.
 # The output should contain 6 lines.
