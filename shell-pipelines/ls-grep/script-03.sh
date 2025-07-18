01996da887fea6ea0dccd62e403f4640fc728d64
aiah ibrahim <ayababikeribrahim@gmail.com>
2025-07-18 17:36:21 +0100

    ls-grep solved

--- shell-pipelines/ls-grep/script-03.sh
+++ shell-pipelines/ls-grep/script-03.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -euo pipefail
-
+ls -1 sample-files | grep '^[A-Z]' | grep -v '.*[A-Z].*[A-Z]'
 # TODO: Write a command to output the names of the files in the sample-files directory whose name starts with an upper case letter and doesn't contain any other upper case letters.
 # Your output should contain 7 files.
+
